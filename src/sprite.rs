1ee26b162deaffb475063288e31d6f00f81d4513
kirinokirino <kirinokirino@lunariko.com>
2022-11-08 17:25:28 +0300

    Split into basic modules.

---
+++ src/sprite.rs
@@ -0,0 +1,66 @@
+use crate::common::{Vec2, Size, max, min};
+use simple_pixels::{rgb::RGBA8, Context};
+
+pub struct Sprite {
+    pub origin: Vec2,
+    size: Size,
+    pixels: Vec<RGBA8>,
+}
+
+impl Sprite {
+    pub fn new(pos: Vec2, size: Size, pixels: Vec<RGBA8>) -> Self {
+        Self {
+            origin: pos,
+            size,
+            pixels,
+        }
+    }
+    pub fn draw(&self, ctx: &mut Context) {
+        let screen_width = ctx.width();
+        let screen_height = ctx.height();
+
+        let screen_size = Size::new(screen_width, screen_height);
+        let screen_origin = Vec2::new(0.0, 0.0);
+
+        let visible_from_x = max(screen_origin.x as i32, self.origin.x as i32);
+        let visible_to_x = min(
+            self.size.width as i32 + self.origin.x as i32,
+            screen_size.width as i32 + screen_origin.x as i32,
+        );
+        let visible_width = visible_to_x - visible_from_x;
+        let sprite_offset_x = if self.origin.x < screen_origin.x {
+            -(self.origin.x - screen_origin.x) as i32
+        } else {
+            0
+        };
+
+        let visible_from_y = max(screen_origin.y as i32, self.origin.y as i32);
+        let visible_to_y = min(
+            self.size.height as i32 + self.origin.y as i32,
+            screen_size.height as i32 + screen_origin.y as i32,
+        );
+        let visible_height = visible_to_y - visible_from_y;
+        let sprite_offset_y = if self.origin.y < screen_origin.y {
+            -(self.origin.y - screen_origin.y) as i32
+        } else {
+            0
+        };
+
+        let mut visible_pixels: Vec<RGBA8> =
+            Vec::with_capacity((visible_width * visible_height).try_into().unwrap());
+
+        for col in sprite_offset_y..visible_height + sprite_offset_y {
+            for row in sprite_offset_x..visible_width + sprite_offset_x {
+                visible_pixels.push(self.pixels[((row * self.size.width as i32) + col) as usize]);
+            }
+        }
+        //dbg!(&visible_width, &visible_height);
+        ctx.draw_pixels(
+            max(screen_origin.x as i32, self.origin.x as i32),
+            max(screen_origin.y as i32, self.origin.y as i32),
+            visible_width.try_into().unwrap(),
+            visible_height.try_into().unwrap(),
+            &visible_pixels,
+        );
+    }
+}
